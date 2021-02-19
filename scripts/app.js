2f19d33010ca245d1ff1d3994e2279e171cd692f
Hannah Tinacci <hannahtinacci@gmail.com>
2021-02-19 18:41:31 +0000

    grid, icon and key events added

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,10 @@ function init() {
   const width = 10
   const cellCount = width * width
   const cells = []
-  console.log('CELLS', cells)
+
+  const bearClass = 'bear'
+  const bearStartPosition = 94
+  let bearCurrentPosition = 94
 
 // * GRID
 
@@ -17,12 +20,44 @@ function init() {
       grid.appendChild(cell)
       cells.push(cell)
     }
-
+    addBear(bearStartPosition)
   }
 
+// * Add bear (aka Frogger) to cell
+  function addBear(position) {
+    cells[position].classList.add(bearClass)
+  }
+
+// * Remove bear (aka Frogger) from cell
+  function removeBear(position) {
+    cells[position].classList.remove(bearClass)
+  }
+
+// * Move bear 
+  function handleKeyUp(event) {
+    const key = event.keyCode
+    
+    removeBear(bearCurrentPosition)
+
+    if (key === 39 && bearCurrentPosition % width -1) {
+      bearCurrentPosition++
+    } else if (key === 37 && bearCurrentPosition % width !== 0) {
+      bearCurrentPosition--
+    } else if (key === 38 && bearCurrentPosition >= width) {
+      bearCurrentPosition -= width
+    } else if (key === 40 && bearCurrentPosition + width <= width * width - 1) {
+      bearCurrentPosition += width
+    } else {
+      console.log('INVALID KEY')
+    }
+
+    addBear(bearCurrentPosition)
+  }
 
 // * EVENT LISTENERS
 
+  document.addEventListener('keyup', handleKeyUp)
+  
   createGrid()
 
 
